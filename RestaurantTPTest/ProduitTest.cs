ab2c7d6debf6f531ecf87a924a4332029305b61e
ljoub <45665062+ljoub@users.noreply.github.com>
2022-02-02 17:25:17 +0100

    maj produit test aucun sens string

--- RestaurantTPTest/ProduitTest.cs
+++ RestaurantTPTest/ProduitTest.cs
@@ -7,18 +7,24 @@ namespace RestaurantTPTest
     public class ProduitTest
     {
         [Fact]
-        public void Statut_d_un_produit_non_pret()
+        public void Produit_bien_créé_avec_statut_non_pret()
         {
-            var produit = new ProduitBuilder().Créé(10, "Produit", 10).Build();
+            var produit = new ProduitBuilder().Créé(10, "Produit", 100).Build();
 
+            Assert.Equal(10, produit.Prix);
+            Assert.Equal("Produit", produit.Nom);
+            Assert.Equal(100, produit.Stock);
             Assert.Equal("non prêt", produit.Statut);
         }
 
         [Fact]
-        public void Statut_de_plusieurs_produits_non_pret()
+        public void Produits_bien_créés_avec_statut_non_pret()
         {
-            var desProduits = new ProduitGenerator().Créés(10, "Produit", 10).Generate(250);
+            var desProduits = new ProduitGenerator().Créés(10, "Produit", 100).Generate(250);
 
+            Assert.Equal(2500, desProduits.Sum(produit => produit.Prix));
+            Assert.Equal("Produit", desProduits.Sum(produit => produit.Nom));
+            Assert.Equal(25000, desProduits.Sum(produit => produit.Stock));
             Assert.Equal("non prêt", desProduits.Sum(produit => produit.Statut));
         }
     }
