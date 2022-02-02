ad18db10e2f27134fa34ce2e938e5b9ab845b4df
FlorentOddo <44065546+FlorentOddo@users.noreply.github.com>
2022-02-02 17:53:29 +0100

    add cuisine test

---
+++ RestaurantTPTest/utilities/CuisineBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestaurantTP;
+namespace RestaurantTPTest.utilities
+{
+    class CuisineBuilder
+    {
+        private Restaurant _resto;
+
+        public CuisineBuilder  Creer(string name)
+        {
+            _resto = new Restaurant(name);
+            return this;
+        }
+        public Cuisine Build()
+        {
+            return new Cuisine(_resto);
+        }
+    }
+}
