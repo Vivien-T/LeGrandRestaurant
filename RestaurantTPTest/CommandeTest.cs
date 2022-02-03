f14c320f7fc649419cb4bd834b62461735f57c29
Vivien-T <thiebaut.vivien@gmail.com>
2022-02-03 18:55:47 +0100

    Update CommandeTest.cs

--- RestaurantTPTest/CommandeTest.cs
+++ RestaurantTPTest/CommandeTest.cs
@@ -53,5 +53,48 @@ namespace RestaurantTPTest
             List<Produit> produitsFinaux = new List<Produit>() { lasagnes, tartes, spaghettis, salade };
             Assert.Equal(produitsFinaux, commandeTest.LesProduits);
         }
+
+        [Fact]
+        public void EnleverUnProduit()
+        {
+            //TODO
+            var commandeTest = new CommandeBuilder().Commandé(_client, _serveur).Build();
+            Produit lasagnes = new Produit(100, "Lasagnes", 6);
+            Produit tartes = new Produit(100, "tartes", 35);
+            Produit spaghettis = new Produit(23, "spaghettis", 100);
+            Produit salade = new Produit(60, "salade", 10);
+            List<Produit> lesPropro = new List<Produit>() { lasagnes, tartes, spaghettis, salade };
+            commandeTest.AjouterDesProduits(lesPropro);
+            Assert.Equal(lesPropro, commandeTest.LesProduits);
+        }
+
+        [Fact]
+        public void EnleverDesProduits()
+        {
+            //TODO
+            var commandeTest = new CommandeBuilder().Commandé(_client, _serveur).Build();
+            Produit lasagnes = new Produit(100, "Lasagnes", 6);
+            Produit tartes = new Produit(100, "tartes", 35);
+            Produit spaghettis = new Produit(23, "spaghettis", 100);
+            Produit salade = new Produit(60, "salade", 10);
+            List<Produit> lesPropro = new List<Produit>() { lasagnes, tartes, spaghettis, salade };
+            commandeTest.AjouterDesProduits(lesPropro);
+            Assert.Equal(lesPropro, commandeTest.LesProduits);
+        }
+
+        [Fact]
+        public void SommeDesProduits()
+        {
+            //TODO
+            var commandeTest = new CommandeBuilder().Commandé(_client, _serveur).Build();
+            Produit lasagnes = new Produit(100, "Lasagnes", 6);
+            Produit tartes = new Produit(100, "tartes", 35);
+            Produit spaghettis = new Produit(23, "spaghettis", 100);
+            Produit salade = new Produit(60, "salade", 10);
+            List<Produit> lesPropro = new List<Produit>() { lasagnes, tartes, spaghettis, salade };
+            commandeTest.AjouterDesProduits(lesPropro);
+            Assert.Equal(lesPropro, commandeTest.LesProduits);
+        }
+
     }
 }
