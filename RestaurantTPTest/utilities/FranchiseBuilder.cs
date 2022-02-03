dda400fec0debfbe78cb837539fb0f014b005be6
GasparTissier <gaspar.tissier@epsi.fr>
2022-02-03 18:54:27 +0100

    Marie joseph

---
+++ RestaurantTPTest/utilities/FranchiseBuilder.cs
@@ -0,0 +1,23 @@
+using RestaurantTP;
+
+namespace RestaurantTPTest.utilities
+{
+  internal class FranchiseBuilder
+  {
+    private string _nom = string.Empty;
+    private List<Restaurant> _listResto = new List<Restaurant>() ;
+
+    public FranchiseBuilder Créé( string nom , List<Restaurant> listResto)
+    {
+      _nom = nom;
+      _listResto = listResto;
+
+      return this;
+    }
+
+    public Franchise Build()
+    {
+      return new Franchise( _nom , _listResto);
+    }
+  }
+}
